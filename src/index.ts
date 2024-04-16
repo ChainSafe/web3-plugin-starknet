2d7ee4f91acb0bd2ab7030dbeda77aa1cd77a168
Bernard Stojanović <bero4net@gmail.com>
2024-04-16 12:32:05 +0200

    Revert "chore: methods mocks"

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,16 @@
 import { Web3PluginBase } from "web3";
-import { StarknetRpcApi } from "./types";
-import * as _methods from "./methods";
 
-export class StarknetPlugin extends Web3PluginBase<StarknetRpcApi> {
+export class StarknetPlugin extends Web3PluginBase {
   public pluginNamespace = "starknet";
 
-  public call(): Object {
-    return _methods.call(this.requestManager);
-  }
-
-  public estimateFee(): Object {
-    return _methods.estimateFee(this.requestManager);
-  }
-
-  public estimateMessageFee(): Object {
-    return _methods.estimateMessageFee(this.requestManager);
-  }
-
-  public simulateTransactions(): Object {
-    return _methods.simulateTransactions(this.requestManager);
-  }
-
-  public traceBlockTransactions(): Object {
-    return _methods.traceBlockTransactions(this.requestManager);
-  }
-
-  public getClassAt(): Object {
-    return _methods.getClassAt(this.requestManager);
-  }
-
-  public getClassHashAt(): Object {
-    return _methods.getClassHashAt(this.requestManager);
-  }
-
-  public getNonce(): Object {
-    return _methods.getNonce(this.requestManager);
-  }
-
-  public getStorageAt(): Object {
-    return _methods.getStorageAt(this.requestManager);
-  }
-
-  public getTransactionByBlockIdAndIndex(): Object {
-    return _methods.getTransactionByBlockIdAndIndex(this.requestManager);
-  }
-
-  public getTransactionByHash(): Object {
-    return _methods.getTransactionByHash(this.requestManager);
+  public test(param: string): void {
+    console.log(param);
   }
 }
 
 // Module Augmentation
 declare module "web3" {
   interface Web3Context {
-    starknet: StarknetPlugin;
+    template: StarknetPlugin;
   }
 }
